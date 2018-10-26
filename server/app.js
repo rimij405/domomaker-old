fcbe9afd79315f12ca35e08142f5ccfb1f2ac4bd
Ian Effendi <effendiian@gmail.com>
2018-10-26 16:54:11 -0400

    Add redis store.

--- server/app.js
+++ server/app.js
@@ -8,6 +8,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
 
 // Set port.
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
@@ -22,6 +24,18 @@ mongoose.connect(db, (err) => {
     throw err;
   }
 });
+
+let redisURL = {
+  hostname: 'localhost',
+  port: 6379
+};
+
+let redisPASS;
+
+if(process.env.REDISCLOUD_URL) {
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  redisPASS = redisURL.auth.split(':')[1];
+}
 
 // Pull in the router.
 const router = require('./router.js');
@@ -38,6 +52,11 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(session({
   key: 'sessionid',
+  store: new RedisStore({
+    host: redisURL.hostname,
+    port: redisURL.port,
+    pass: redisPASS
+  }),
   secret: 'iae2784 - Domo Arigato',
   resave: 'true',
   saveUninitialized: true,
